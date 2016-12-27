ba4a1dc187ac486d4d46465f416e23face22ef37
tmpbook <nauy2011@126.com>
2016-12-27 18:28:05 +0800

    demo in meeting

--- src/components/TopNav.vue
+++ src/components/TopNav.vue
@@ -1,12 +1,12 @@
 <template>
   <div>
-    <el-menu default-active="1" class="el-menu-demo topnav" mode="horizontal" :router=true theme="dark">
-      <el-menu-item index="home">Home</el-menu-item>
-      <el-menu-item index="question" to="">问答</el-menu-item>
+    <el-menu default-active="1" class="el-menu-demo topnav" mode="horizontal" :router=true>
+      <el-menu-item index="/home">Home</el-menu-item>
+      <el-menu-item index="/question" to="">问答</el-menu-item>
       <el-submenu index="2">
         <template slot="title">第三方组件</template>
-        <el-menu-item index="charts">Echarts</el-menu-item>
-        <el-menu-item index="login">Not Found</el-menu-item>
+        <el-menu-item index="/charts">Echarts</el-menu-item>
+        <el-menu-item index="/login">Not Found</el-menu-item>
       </el-submenu>
     </el-menu>
     <router-view></router-view>
--- src/pages/charts/index.vue
+++ src/pages/charts/index.vue
@@ -1,14 +1,26 @@
 <template>
   <div>
-    <el-row>
-      <el-col :span="12">
-        <chart ref="p" :options="polar"></chart>
+    <el-row v-loading.fullscreen.lock="fullscreenLoading">
+      <el-col :span="18" v-if="keys">
+        <el-row>
+          <el-col :span="24">
+            <chart v-if=polar :options=polar ref="p"></chart>
+          </el-col>
+          <el-col :span="24">
+            <hr>
+            <el-radio-group v-model="state">
+              <el-radio-button v-for="key in keys" :label=key></el-radio-button>
+            </el-radio-group>
+            <hr>
+          </el-col>
+        </el-row>
       </el-col>
-      <el-col :span="12">
+      <el-col :span="6">
         <el-card class="box-card">
           <div slot="header" class="clearfix">
-            <span style="line-height: 36px;">OPTIONS</span>
-            <el-button style="float: right;" type="primary" @click="changeOption">Change Options</el-button>
+            <span style="line-height: 36px;">{{ state }}</span>
+            <br>
+            <span style="float: right;">{{ tips }}</span>
           </div>
           <pre class='polar_code'>{{ polar }}</pre>
         </el-card>
@@ -19,13 +31,13 @@
 
 <style>
 .polar_code {
-  font-size: 10px;
+  font-size: 15px;
 }
 </style>
 
 <script>
-// import axios from 'axios'
-// import _ from 'lodash'
+import axios from 'axios'
+import _ from 'lodash'
 import Vue from 'vue'
 import ECharts from 'vue2-echarts/src/ECharts/ECharts.vue'
 Vue.component('chart', ECharts)
@@ -34,205 +46,49 @@ import 'nprogress/nprogress.css'
 export default {
   data () {
     return {
-      polar: {
-        title: {
-          text: 'initial title',
-          subtext: '纯属虚构',
-          x: 'left'
-        },
-        tooltip: {
-          trigger: 'item',
-          formatter: '{a} <br/>{b}: {c} ({d}%)'
-        },
-        legend: {
-          orient: 'vertical',
-          x: 'left',
-          y: 'bottom',
-          // data: data['data']
-          data: ['mysql-5.5', 'percona-5.5']
-        },
-        grid: {
-          left: '3%',
-          right: '4%',
-          bottom: '30%',
-          containLabel: false
-        },
-        series: [
-          {
-            name: '数据库类型',
-            type: 'pie',
-            selectedMode: 'single',
-            radius: [0, '40%'],
-
-            label: {
-              normal: {
-                show: false
-                // position: 'inner'
-              }
-            },
-            labelLine: {
-              normal: {
-                show: false
-              }
-            },
-            // data: data['data1']
-            data: [{
-              name: 'MySQL',
-              value: 9449
-            },
-            {
-              name: 'MongoDB',
-              value: 641
-            }]
-          },
-          {
-            name: 'MySQL是否高可用',
-            type: 'pie',
-            radius: ['45%', '65%'],
-
-            label: {
-              normal: {
-                show: false
-                // position: 'inner'
-              }
-            },
-            labelLine: {
-              normal: {
-                show: false
-              }
-            },
-
-            // data: data['data2']
-            data: [{
-              name: 'Nomal',
-              value: 6980
-            },
-            {
-              name: 'HA',
-              value: 2469
-            }]
-          },
-          {
-            name: '数据库版本',
-            type: 'pie',
-            radius: ['70%', '80%'],
-
-            // data: data['data3']
-            data: [{
-              name: 'mysql-5.5',
-              value: 4946
-            },
-            {
-              name: 'percona-5.5',
-              value: 135
-            }]
-          }
-        ]
-      }
+      state: 'AK',
+      keys: null,
+      polar: null,
+      fullscreenLoading: true,
+      tips: 'Pick one...'
+    }
+  },
+  watch: {
+    state () {
+      this.tips = 'Waiting...'
+      this.loadStateData(this.state)
     }
   },
   methods: {
     changeOption () {
-      this.polar = {
-        title: {
-          text: 'title chenged',
-          subtext: '纯属虚构',
-          x: 'left'
-        },
-        tooltip: {
-          trigger: 'item',
-          formatter: '{a} <br/>{b}: {c} ({d}%)'
-        },
-        legend: {
-          orient: 'vertical',
-          x: 'left',
-          y: 'bottom',
-          // data: data['data']
-          data: ['mysql-5.5', 'percona-5.5']
-        },
-        grid: {
-          left: '3%',
-          right: '4%',
-          bottom: '30%',
-          containLabel: false
-        },
-        series: [
-          {
-            name: '数据库类型',
-            type: 'pie',
-            selectedMode: 'single',
-            radius: [0, '40%'],
-
-            label: {
-              normal: {
-                show: false
-                // position: 'inner'
-              }
-            },
-            labelLine: {
-              normal: {
-                show: false
-              }
-            },
-            // data: data['data1']
-            data: [{
-              name: 'MySQL',
-              value: 94490
-            },
-            {
-              name: 'MongoDB',
-              value: Math.random() * 10000
-            }]
-          },
-          {
-            name: 'MySQL是否高可用',
-            type: 'pie',
-            radius: ['55%', '65%'],
-
-            label: {
-              normal: {
-                show: false
-                // position: 'inner'
-              }
-            },
-            labelLine: {
-              normal: {
-                show: false
-              }
-            },
-
-            // data: data['data2']
-            data: [{
-              name: 'Nomal',
-              value: 6980
-            },
-            {
-              name: 'HA',
-              value: Math.random() * 1000
-            }]
-          },
-          {
-            name: '数据库版本',
-            type: 'pie',
-            radius: ['70%', '80%'],
-
-            // data: data['data3']
-            data: [{
-              name: 'mysql-5.5',
-              value: Math.random() * 100
-            },
-            {
-              name: 'percona-5.5',
-              value: 135
-            }]
-          }
-        ]
-      }
       this.polar.title.subtext = Math.random()
     },
     resizeMyself () {
       this.$refs.p.resize()
-    }
+    },
+    loadStateData:
+      _.debounce(
+        function (state) {
+          let vm = this
+          vm.tips = 'Loading...'
+          axios.get('http://127.0.0.1:8000/quandl_analysis/test/', {
+            params: {
+              'state': state
+            }
+          })
+          .then(function (response) {
+            vm.polar.title.text = response.data.title
+            vm.polar.xAxis.data = response.data.index
+            vm.polar.series.data = response.data.value
+            vm.polar.series.name = response.data.title
+            vm.tips = 'Done...'
+          })
+          .catch(function (error) {
+            console.log(error)
+          })
+        },
+      1000
+      )
   },
   beforeCreate () {
     console.log('Charts Component beforeCreate!')
@@ -240,19 +96,90 @@ export default {
   },
   created () {
     console.log('Charts Component created!')
-    console.log('data: ' + this.polar.legend.data)
-    // var data = {}
-    // axios.get('http://127.0.0.1:8000/outer/')
-    //   .then(function (response) {
-    //     var data = response
-    //     console.log(data)
-    //   })
-    //   .catch(function (error) {
-    //     data = error
-    //     // console.log(data)
-    //   })
   },
   mounted () {
+    var vm = this
+    axios.get('http://127.0.0.1:8000/quandl_analysis/test/', {
+      params: {
+        'state': this.state
+      }
+    })
+    .then(function (response) {
+      vm.keys = response.data.key
+      vm.polar = {
+        title: {
+          text: response.data.title
+        },
+        tooltip: {
+          trigger: 'axis'
+        },
+        xAxis: {
+          data: response.data.index
+        },
+        yAxis: {
+          splitLine: {
+            show: false
+          }
+        },
+        toolbox: {
+          left: 'center',
+          feature: {
+            dataZoom: {
+              yAxisIndex: 'none'
+            },
+            restore: {},
+            saveAsImage: {}
+          }
+        },
+        dataZoom: [{
+          startValue: '1978-08-31T00:00:00'
+        }, {
+          type: 'inside'
+        }],
+        visualMap: {
+          top: 10,
+          right: 10,
+          pieces: [{
+            gt: 0,
+            lte: 50,
+            color: '#096'
+          }, {
+            gt: 50,
+            lte: 100,
+            color: '#ffde33'
+          }, {
+            gt: 100,
+            lte: 150,
+            color: '#ff9933'
+          }, {
+            gt: 150,
+            color: '#7e0023'
+          }],
+          outOfRange: {
+            color: '#999'
+          }
+        },
+        series: {
+          name: response.data.title,
+          type: 'line',
+          data: response.data.value,
+          markLine: {
+            silent: true,
+            data: [{
+              yAxis: 50
+            }, {
+              yAxis: 100
+            }, {
+              yAxis: 150
+            }]
+          }
+        }
+      }
+      vm.fullscreenLoading = false
+    })
+    .catch(function (error) {
+      console.log(error)
+    })
     console.log('Charts Component mounted!')
     NProgress.done()
   },
